3162a1f679e138809ccb8101249f945cef28625b
Kio-Kanata <4523119@ed.tus.ac.jp>
2025-09-09 05:50:35 +0900

    mod node

--- ROS/src/motor_control_serial/motor_control_serial/motor_control_serial_node.py
+++ ROS/src/motor_control_serial/motor_control_serial/motor_control_serial_node.py
@@ -1,6 +1,10 @@
-def main():
-    print('Hi from motor_control_serial.')
+from time import sleep
 
+def main():
+    i = 0
+    while True:
+        print(f"Hello, World! {i}")
+        sleep(1)
 
 if __name__ == '__main__':
     main()
