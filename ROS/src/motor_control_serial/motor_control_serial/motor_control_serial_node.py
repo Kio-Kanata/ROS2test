69c0c2089d9806071fdafbb9af007a20949cc14a
Kio-Kanata <4523119@ed.tus.ac.jp>
2025-09-10 19:19:04 +0900

    mod

--- ROS/src/motor_control_serial/motor_control_serial/motor_control_serial_node.py
+++ ROS/src/motor_control_serial/motor_control_serial/motor_control_serial_node.py
@@ -1,3 +1,7 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
+
 from time import sleep
 import serial
 
@@ -16,75 +20,86 @@ class commData:
         self.wheel = motorVal()
         self.steer = motorVal()
 
-ser = serial.Serial("/dev/ttyACM0", 115200)
+class Serial4wheel4steer(Node):
+    def __init__(self):
+        self.pub = self.create_publisher(Float64MultiArray, '/motor/actual_val', 10)
+        self.sub = self.create_subscription(Float64MultiArray, '/rover/targets', self.motor_cb, 10)
+        self.ser = serial.Serial("/dev/ttyACM0", 115200)
+        self.enc = dataEncoder.dataEncoder(0)
+        self.dec = dataEncoder.dataDecoder()
+        self.sendData = commData()
+        self.recvData = commData()
+        self.encoderInit()
+        self.timer = self.create_timer(0.1, self.loop)
+    
+    def encoderInit(self):
+        self.enc.append(0, self.sendData.wakeUp)
+        self.enc.append(1, self.sendData.wheel.FL)
+        self.enc.append(2, self.sendData.wheel.RL)
+        self.enc.append(3, self.sendData.wheel.RR)
+        self.enc.append(4, self.sendData.wheel.FR)
+        self.enc.append(5, self.sendData.steer.FL)
+        self.enc.append(6, self.sendData.steer.RL)
+        self.enc.append(7, self.sendData.steer.RR)
+        self.enc.append(8, self.sendData.steer.FR)
+        self.enc.init()
+        dataPacket = self.enc.getPacket()
+        self.ser.write(bytes(dataPacket.toInit.data))
+        for data in dataPacket.toInit.data:
+            self.dec.appendToBuffer(data)
+        self.dec.decode()
 
-enc = dataEncoder.dataEncoder(0)
-dec = dataEncoder.dataDecoder()
-sendData = commData()
-recvData = commData()
+    def send(self):
+        self.enc.encode()
+        dataPacket = self.enc.getPacket()
+        self.ser.write(bytes(dataPacket.toSend.data))
 
-def encoderInit():
-    enc.append(0, sendData.wakeUp)
-    enc.append(1, sendData.wheel.FL)
-    enc.append(2, sendData.wheel.RL)
-    enc.append(3, sendData.wheel.RR)
-    enc.append(4, sendData.wheel.FR)
-    enc.append(5, sendData.steer.FL)
-    enc.append(6, sendData.steer.RL)
-    enc.append(7, sendData.steer.RR)
-    enc.append(8, sendData.steer.FR)
-    enc.init()
-    dataPacket = enc.getPacket()
-    ser.write(bytes(dataPacket.toInit.data))
-    for data in dataPacket.toInit.data:
-        dec.appendToBuffer(data)
-    dec.decode()
+    def recv(self):
+        dataPacket = self.enc.getPacket()
+        data_num = self.ser.in_waiting
+        if data_num >= dataPacket.toSend.length:
+            buff = self.ser.read(data_num)
+            for data in buff:
+                self.dec.appendToBuffer(data)        
+            self.dec.decode()
+            self.recvData.wakeUp[0] = self.dec.decodedData(0, 0)
+            self.recvData.wheel.FL[0] = self.dec.decodedData(0, 1)
+            self.recvData.wheel.RL[0] = self.dec.decodedData(0, 2)
+            self.recvData.wheel.RR[0] = self.dec.decodedData(0, 3)
+            self.recvData.wheel.FR[0] = self.dec.decodedData(0, 4)
+            self.recvData.steer.FL[0] = self.dec.decodedData(0, 5)
+            self.recvData.steer.RL[0] = self.dec.decodedData(0, 6)
+            self.recvData.steer.RR[0] = self.dec.decodedData(0, 7)
+            self.recvData.steer.FR[0] = self.dec.decodedData(0, 8)
 
-def send():
-    enc.encode()
-    dataPacket = enc.getPacket()
-    ser.write(bytes(dataPacket.toSend.data))
+    def printData(self):
+        print(self.recvData.wakeUp, self.recvData.wheel.FL, self.recvData.wheel.RL, self.recvData.wheel.RR, self.recvData.wheel.FR, self.recvData.steer.FL, self.recvData.steer.RL, self.recvData.steer.RR, self.recvData.steer.FR)
 
-def recv():
-    dataPacket = enc.getPacket()
-    data_num = ser.in_waiting
-    if data_num >= dataPacket.toSend.length:
-        buff = ser.read(data_num)
-        for data in buff:
-            dec.appendToBuffer(data)        
-        dec.decode()
-        recvData.wakeUp[0] = dec.decodedData(0, 0)
-        recvData.wheel.FL[0] = dec.decodedData(0, 1)
-        recvData.wheel.RL[0] = dec.decodedData(0, 2)
-        recvData.wheel.RR[0] = dec.decodedData(0, 3)
-        recvData.wheel.FR[0] = dec.decodedData(0, 4)
-        recvData.steer.FL[0] = dec.decodedData(0, 5)
-        recvData.steer.RL[0] = dec.decodedData(0, 6)
-        recvData.steer.RR[0] = dec.decodedData(0, 7)
-        recvData.steer.FR[0] = dec.decodedData(0, 8)
+    def setVal(self, msg):
+        self.sendData.wheel.FL[0] = msg.data[1]
+        self.sendData.wheel.RL[0] = msg.data[2]
+        self.sendData.wheel.RR[0] = msg.data[3]
+        self.sendData.wheel.FR[0] = msg.data[4]
+        self.sendData.steer.FL[0] = msg.data[5]
+        self.sendData.steer.RL[0] = msg.data[6]
+        self.sendData.steer.RR[0] = msg.data[7]
+        self.sendData.steer.FR[0] = msg.data[8]
+    
+    def motor_cb(self, msg):
+        self.setVal(msg)
+        pub_data = Float64MultiArray([self.recvData.wheel.FL[0], self.recvData.wheel.RL[0], self.recvData.wheel.RR[0], self.recvData.wheel.FR[0], self.recvData.steer.FL[0], self.recvData.steer.RL[0], self.recvData.steer.RR[0], self.recvData.steer.FR[0]])
+        self.pub.publish(pub_data)
+    
+    def loop(self):
+        self.recv()
+        self.printData()
+        self.sendData.wakeUp[0] = not self.recvData.wakeUp[0]
+        self.send()
 
-def printData():
-    print(recvData.wakeUp, recvData.wheel.FL, recvData.wheel.RL, recvData.wheel.RR, recvData.wheel.FR, recvData.steer.FL, recvData.steer.RL, recvData.steer.RR, recvData.steer.FR)
-
-def setData():
-    sendData.wakeUp[0] = not recvData.wakeUp[0]
-    sendData.wheel.FL[0] = 0.0
-    sendData.wheel.RL[0] = 0.0
-    sendData.wheel.RR[0] = 10.0
-    sendData.wheel.FR[0] = 0.0
-    sendData.steer.FL[0] = 0.0
-    sendData.steer.RL[0] = 0.0
-    sendData.steer.RR[0] = 0.0
-    sendData.steer.FR[0] = 0.0
-
-def main():
-    encoderInit()
-    while True:
-        recv()
-        printData()
-        setData()
-        send()
-        sleep(0.1)
+def main(args=None):
+    rclpy.init(args=args)
+    node = Serial4wheel4steer()
+    rclpy.spin(node)
 
 if __name__ == '__main__':
     main()
